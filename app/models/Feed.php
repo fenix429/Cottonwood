e111f8c5e35ef3424245a171da666f680cd7cbe6
fenix429 <fenix429@gmail.com>
2013-10-01 04:56:00 -0400

    setup article model and relationships, namespaced models

--- app/models/Feed.php
+++ app/models/Feed.php
@@ -1,7 +1,16 @@
 <?php
 
+namespace Models;
+
+use \Eloquent;
+
 class Feed extends Eloquent {
 	protected $guarded = array('id');
 
 	public static $rules = array();
+	
+    public function articles()
+    {
+        return $this->hasMany('Models\Article');
+    }
 }
