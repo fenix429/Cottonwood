23173857d9d34d1b681902f804748c47b6f07f40
fenix429 <fenix429@gmail.com>
2013-10-04 03:37:08 -0400

    cleaned up uneeded actions on the resource routes

--- app/controllers/ArticleController.php
+++ app/controllers/ArticleController.php
@@ -1,13 +1,12 @@
 <?php
 
-class ArticleController extends BaseController {
-	
-	// Dummy content
-	var $articles = array();
-	
-	public function __construct()
+use Cottonwood\Storage\Article\ArticleRepository;
+
+class ArticleController extends BaseController
+{
+	public function __construct(ArticleRepository $repository)
 	{
-		
+		$this->repository = $repository;
 	}
 	
 	/**
@@ -17,29 +16,9 @@ class ArticleController extends BaseController {
 	 */
 	public function index($feedId)
 	{
-		$articles = Models\Feed::find($feedId)->articles()->where('unread', TRUE)->get();
+		$articles = $this->repository->findByFeed($feedId);
 		
 		return Response::make($articles->toJSON(), 200)->header('Content-Type', 'application/json');
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create($feedId)
-	{
-		//
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store($feedId)
-	{
-		
 	}
 
 	/**
@@ -48,42 +27,11 @@ class ArticleController extends BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function show($feedId, $taskId)
+	public function show($feedId, $articleId)
 	{
+		$article = $this->repository->find($articleId);
 		
-	}
-
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($feedId, $taskId)
-	{
-		
-	}
-
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($feedId, $taskId)
-	{
-		
-	}
-
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($feedId, $taskId)
-	{
-		
+		return Response::make($article->toJSON(), 200)->header('Content-Type', 'application/json');
 	}
 
 }
