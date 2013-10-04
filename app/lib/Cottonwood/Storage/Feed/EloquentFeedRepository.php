e23a84c6ce8cad5101e2181ca4e36359e2259628
fenix429 <fenix429@gmail.com>
2013-10-04 00:35:23 -0400

    added comments

--- app/lib/Cottonwood/Storage/Feed/EloquentFeedRepository.php
+++ app/lib/Cottonwood/Storage/Feed/EloquentFeedRepository.php
@@ -3,41 +3,74 @@
 namespace Cottonwood\Storage\Feed;
 
 use Models\Feed;
-//use Models\User;
 
 class EloquentFeedRepository implements FeedRepository
 {
+    /**
+	 * Find record by id
+	 *
+	 * @param int
+	 * @return Models\Feed 
+	 */
     public function find($id)
     {
         return Feed::findOrFail($id);
     }
     
+    /**
+	 * Find all records
+	 * 
+	 * @return Models\Feed Collection
+	 */
     public function findAll()
     {
         return Feed::all();
     }
     
+    /**
+	 * Find records by related user
+	 *
+	 * @param int
+	 * @return Models\Feed Collection
+	 */
     public function findByUserId($userId)
     {
         // -> oauth - ?
     }
     
+    /**
+	 * Create record
+	 * 
+	 * @param array
+	 * @return Models\Feed 
+	 */
     public function create($input)
     {
         return Feed::create($input);
     }
     
+    /**
+	 * Update record
+	 *
+	 * @param int
+	 * @param array
+	 * @return Models\Feed 
+	 */
     public function update($id, $input)
     {
         $feed = Feed::find($id);
-		
 		$feed->fill($input);
-		
 		$feed->save();
 		
 		return $feed;
     }
     
+    /**
+	 * Delete record
+	 *
+	 * @param int
+	 * @return void
+	 */
     public function destroy($id)
     {
         Feed::destroy($id);
