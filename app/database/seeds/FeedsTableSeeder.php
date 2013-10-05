ea973def97fce40e968924dbf4a021c707e3d5fb
fenix429 <fenix429@gmail.com>
2013-10-05 16:53:08 -0400

    Seeder now adds created_at and updated_at correctly

--- app/database/seeds/FeedsTableSeeder.php
+++ app/database/seeds/FeedsTableSeeder.php
@@ -6,19 +6,24 @@ class FeedsTableSeeder extends Seeder {
 	{
 	    DB::statement("SET FOREIGN_KEY_CHECKS=0;");
 	    
-		DB::table('feeds')->truncate();
+		DB::table("feeds")->truncate();
         
         DB::statement("SET FOREIGN_KEY_CHECKS=1;");
         
 		$feeds = array(
-			array("title" => "ArsTechnica", "url" => "http://feeds.arstechnica.com/arstechnica/index"),
-			array("title" => "ReadWrite",   "url" => "http://www.readwriteweb.com/rss.xml"),
-			array("title" => "TechCrunch",  "url" => "http://feeds.feedburner.com/TechCrunch/"),
-			array("title" => "io9",         "url" => "http://feeds.gawker.com/io9/full"),
-			array("title" => "Slashdot",    "url" => "http://rss.slashdot.org/Slashdot/slashdot")
+			["title" => "ArsTechnica", "url" => "http://feeds.arstechnica.com/arstechnica/index"],
+			["title" => "ReadWrite",   "url" => "http://www.readwriteweb.com/rss.xml"],
+			["title" => "TechCrunch",  "url" => "http://feeds.feedburner.com/TechCrunch/"],
+			["title" => "io9",         "url" => "http://feeds.gawker.com/io9/full"],
+			["title" => "Slashdot",    "url" => "http://rss.slashdot.org/Slashdot/slashdot"]
 		);
+		
+		array_walk($feeds, function(&$feed)
+		{
+    		$feed = array_merge($feed, [ "created_at" => new DateTime(), "updated_at" => new DateTime() ]);
+		});
 
-		DB::table('feeds')->insert($feeds);
+		DB::table("feeds")->insert($feeds);
 	}
 
 }
